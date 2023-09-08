44e36fb1e668caf270dd40bee01e6421663ddbdc
dganesh22 <dganesh2022@gmail.com>
2023-09-08 12:08:41 +0530

    completed api and file read

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express  = require('express')
 require('dotenv').config()
 const cors = require('cors')
 
+const connectDb = require('./db/connect')
 
 const PORT = process.env.PORT
 
@@ -9,6 +10,8 @@ const app  = express()
 
 app.set(`view engine`, 'pug')
 app.set(`views`, './view')
+
+app.use(express.static('./docs'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -19,5 +22,6 @@ app.use(cors())
 app.use(`/`, require('./route/fileRoute'))
 
 app.listen(PORT, () => {
+    connectDb()
     console.log(`server is connected and running @ http://localhost:${PORT}`)
 })
